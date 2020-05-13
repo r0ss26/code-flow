5254fc4412eef4ca9f2e520bed25e36b349e54e8
Ross Baker <rossbaker32@gmail.com>
2020-05-13 15:37:25 +1000

    User Model Update.
    Added columns for name, location and bio to the user model.

---
+++ db/migrate/20200513053421_add_columns_to_user.rb
@@ -0,0 +1,10 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string, {null: false}
+    add_column :users, :middle_name, :string
+    add_column :users, :last_name, :string, {null: false}
+    add_column :users, :city, :string
+    add_column :users, :country, :string
+    add_column :users, :biography, :text
+  end
+end
