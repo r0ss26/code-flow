10d5180e9006d3da491ff612e6537fb516f11b51
Ross Baker <rossbaker32@gmail.com>
2020-05-13 20:32:25 +1000

    User authentication.
    Add permitted parameters for custom user fields.
    Modify sign up form to accept custom inputs.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters if :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :first_name, :middle_name, :last_name, :city, :country, :biography) }
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -83,7 +83,7 @@ end
 for i in 0..50
   order = Order.all.sample
   order.create_review(user_id: order.listing.user_id,
-                       rating: rand(1..5),
-                       description: Faker::Hipster.paragraph(sentence_count: rand(1..4))) unless order.review
+                      rating: rand(1..5),
+                      description: Faker::Hipster.paragraph(sentence_count: rand(1..4))) unless order.review
   puts "Review #{i} created"                
 end
