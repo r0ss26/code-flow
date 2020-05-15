2bba761a9d214fe583ce28e8d1b960516bf739ad
Ross Baker <rossbaker32@gmail.com>
2020-05-15 15:45:15 +1000

    Change root route to home action on pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root to: "listings#index"
+  root to: "pages#home"
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "seller", to: "listings#seller"
   get "buyer", to: "listings#buyer"
+  
   resources :listings do
     resources :orders
   end
