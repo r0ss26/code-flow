ebd68dac75ab3f6a06b4b11080d197136c32f958
Ross Baker <rossbaker32@gmail.com>
2020-05-16 12:06:33 +1000

    Move standalone dashboard routes into the dashboard resource.
    It's more logical to have to dashboard routes in their own controller
    rather than spread out over multiple controllers with hard coded routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   devise_for :users
-  get "seller/listings", to: "listings#seller_listings"
-  get "seller/orders", to: "orders#seller_orders"
-  get "buyer", to: "listings#buyer"
   
   # Dashboard is a singular resource because each user
   # will only have one dashboard.
