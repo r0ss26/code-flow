11e9f50745446bad7b1688f37b23685de8bcc223
Ross Baker <rossbaker32@gmail.com>
2020-05-16 16:59:19 +1000

    Add Order partial for rendering a users purchased orders in the dashboard.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  validates :listing_id, :cost, :buyer_id, :seller_id, presence: true
+  validates :cost, :buyer_id, :seller_id, presence: true
   belongs_to :buyer, class_name: "User"
   belongs_to :seller, class_name: "User"
   belongs_to :listing
