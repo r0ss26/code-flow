5cb32c0339c73f6fbc03804706065668f39e128f
Ross Baker <rossbaker32@gmail.com>
2020-05-18 18:26:50 +1000

    Get the most popular tags to display on the home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @popular_listings = Listing.order("orders_count DESC").limit(3)
+    @tags = ActsAsTaggableOn::Tag.most_used(10)
   end
   
 end
