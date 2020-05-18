e592d02f93e3f82fabb6bf751fac2f71b8a7d5be
Ross Baker <rossbaker32@gmail.com>
2020-05-18 18:27:14 +1000

    Build out the skeleton controller for displaying a users profile.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,26 +1,31 @@
 class ProfilesController < ApplicationController
 
   def show
-    @education_history = Education.all
-    @employment_history = Employment.all
-    @reviews = Review.where(review_receiver_id: current_user.id)
-    @listings = current_user.listings
+    # @education_history = Education.all
+    # @employment_history = Employment.all
+    @user = User.find(params[:user_id])
+    @reviews = Review.where(review_receiver_id: params[:user_id])
+    puts @reviews
+    if !@reviews.empty?
+      @avg_rating = @reviews.average(:rating).round(2) 
+    end
+    @listings = User.find(params[:user_id]).listings
   end
 
   def education
-
+    @educations = User.find(params[:user_id]).educations
   end
 
   def employment
-
+    @employments = User.find(params[:user_id]).employments
   end
 
   def listings
-
+    @listings = User.find(params[:user_id]).listings
   end
 
   def reviews
-
+    @reviews = Review.where(review_receiver_id: params[:user_id])
   end
 
 end
