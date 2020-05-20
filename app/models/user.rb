7cb95851fba514138eff963a9ce67a8fdb6da809
Ross Baker <rossbaker32@gmail.com>
2020-05-20 17:24:08 +1000

    Add deletion dependencies for User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :employments
-  has_many :educations
-  has_many :listings
-  has_many :sales, class_name: "Order", foreign_key: "seller_id"
-  has_many :purchases, class_name: "Order", foreign_key: "buyer_id"
-  has_many :reviews
-  has_many :received_reviews, class_name: "Review", foreign_key: "review_receiver_id"
-  has_many :posted_reviews, class_name: "Review", foreign_key: "review_poster_id"
-  has_one_attached :image
+  has_many :employments, dependent: :destroy
+  has_many :educations, dependent: :destroy
+  has_many :listings, dependent: :destroy
+  has_many :sales, class_name: "Order", foreign_key: "seller_id", dependent: :nullify
+  has_many :purchases, class_name: "Order", foreign_key: "buyer_id", dependent: :nullify
+  has_many :received_reviews, class_name: "Review", foreign_key: "review_receiver_id", dependent: :destroy
+  has_many :posted_reviews, class_name: "Review", foreign_key: "review_poster_id", dependent: :destroy
+  has_one_attached :image, dependent: :destroy
   acts_as_favoritor
 end
