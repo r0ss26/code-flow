e3db9a16d8cbad6161130ac9afebc033f91e47cc
Ross Baker <rossbaker32@gmail.com>
2020-05-20 17:27:00 +1000

    destroy orders insteaf of nullify on user deletion

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
   validates :name, presence: true
-  has_many :listings, dependent: destroy
+  has_many :listings, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   has_many :employments, dependent: :destroy
   has_many :educations, dependent: :destroy
   has_many :listings, dependent: :destroy
-  has_many :sales, class_name: "Order", foreign_key: "seller_id", dependent: :nullify
-  has_many :purchases, class_name: "Order", foreign_key: "buyer_id", dependent: :nullify
+  has_many :sales, class_name: "Order", foreign_key: "seller_id", dependent: :destroy
+  has_many :purchases, class_name: "Order", foreign_key: "buyer_id", dependent: :destroy
   has_many :received_reviews, class_name: "Review", foreign_key: "review_receiver_id", dependent: :destroy
   has_many :posted_reviews, class_name: "Review", foreign_key: "review_poster_id", dependent: :destroy
   has_one_attached :image, dependent: :destroy
